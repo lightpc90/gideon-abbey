52303725f3118d8a0b0a183c03a6b4f8fe2ec26b
lightpc90 <afoageektech@gmail.com>
2023-08-01 14:49:54 +0100

    deleted testimonial component and import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,6 @@ import Layout from '../components/Layout'
 import About from '../components/about/About'
 import Experience from '../components/experience/Experience'
 import Portfolio from '../components/portfolio/Portfolio'
-import Testimonials from '../components/testimonials/Testimonials'
 import Contact from '../components/contact/Contact'
 
 const inter = Inter({ subsets: ['latin'] })
